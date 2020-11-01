7a6217cdd024d53404f320d5086b947bfd270cea
Ludovic Engrand <ludovic.engrand@sap.com>
2020-11-01 19:27:02 +0100

    cleanup

--- out/confidentialite.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-require("bootstrap/dist/css/bootstrap.min.css");
-
-require("../css/main.css");
--- out/icons.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _fontawesomeSvgCore = require("@fortawesome/fontawesome-svg-core");
-
-var _freeSolidSvgIcons = require("@fortawesome/free-solid-svg-icons");
-
-_fontawesomeSvgCore.library.add(_freeSolidSvgIcons.faFilePdf);
-
-_fontawesomeSvgCore.dom.watch();
